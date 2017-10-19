997bf0e9f3123d5c495fbab28e05a8368e31a579
KhanhHoang <1551010037hoang@ou.edu.vn>
2017-10-19 20:05:59 +0700

    Dang lam dang nhap

---
+++ Login.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Login.aspx.cs" Inherits="Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Đăng nhập</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>        
+        <asp:Login  ID="Login1" runat="server" CreateUserUrl="~/Taotaikhoan.aspx"
+                    LoginButtonText="Đăng nhập" OnAuthenticate="Login1_Authenticate"
+                    OnLoggedIn="Login1_LoggedIn" OnLoggingIn="Login1_LoggingIn"
+                    OnLoginError="Login1_LoginError" PasswordLabelText="Mật khẩu:"
+                    PasswordRecoveryUrl="~/RecoveryPass.aspx" RememberMeText="Nhớ đăng nhập cho lần sau"
+                    TitleText="Đăng nhập" UserNameLabelText="Đăng Nhập:" FailureText="Tài khoản hoặc mật khẩu không đúng">
+        </asp:Login>
+    
+        <asp:LinkButton ID="LinkButton4" runat="server" PostBackUrl="~/RecoveryPass.aspx">Quên tài khoản?</asp:LinkButton>
+    
+    </div>
+    
+        <p>
+            <asp:LinkButton ID="LinkButton3" runat="server" PostBackUrl="~/Taotaikhoan.aspx">Tạo tài khoản</asp:LinkButton>
+        </p>
+        
+    </form>
+    <session
+        mode="StateServer"
+        cookieless="false"
+        timeout="2000"/>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+public partial class Login : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //this.Label1.Text = Session["UserName"] as string;
+    }
+
+    protected void Login1_LoggedIn(object sender, EventArgs e)
+    {
+        Session["UserName"] = this.Login1.UserName;
+    }
+
+    protected void Login1_LoggingIn(object sender, LoginCancelEventArgs e)
+    {
+
+    }
+
+    protected void Login1_LoginError(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ToString());
+
+        con.Open();
+
+        string query = "SELECT COUNT(*) FROM UserLogin.[dbo].[User] WHERE (UserName ='" + Login1.UserName.ToString() + "' AND Password ='" + Login1.Password.ToString() + "')";
+        SqlCommand cmd = new SqlCommand(query, con);
+
+        string output = cmd.ExecuteScalar().ToString();
+
+        if (output == "1")
+        {
+            
+           Session["UserName"] = Login1.UserName.ToString();
+            Response.Redirect("Chaomung.aspx");
+            //Response.Write("Dang nhap thanh cong");
+           // e.Authenticated = true;
+        }
+        else
+        {
+            //Response.Write("<script> alert('Dang nhap that bai') </script>");
+            
+           // e.Authenticated = false;
+        }
+
+    }
+
+}
